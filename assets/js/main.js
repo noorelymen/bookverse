c33f0caba7c009297a8909c60800570d60950b9e
noorelymen <noorel.ymen@gmail.com>
2023-10-25 23:03:13 +0100

    Header and search done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,40 @@
 /*=============== SEARCH ===============*/
+const searchButton = document.getElementById("search-button"),
+  searchClose = document.getElementById("search-close"),
+  searchContent = document.getElementById("search-content");
 
+/*===== SHOW SEARCH =====*/
+/* Validate if constant exists */
+if (searchButton) {
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
+}
+
+/*===== Hide SEARCH =====*/
+/* Validate if constant exists */
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
+}
 
 /*=============== LOGIN ===============*/
 
-
 /*=============== ADD SHADOW HEADER ===============*/
-
 
 /*=============== HOME SWIPER ===============*/
 
-
 /*=============== FEATURED SWIPER ===============*/
-
 
 /*=============== NEW SWIPER ===============*/
 
-
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
